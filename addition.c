2a5e6c49c5a3c53f0cd0d0a4f6ad437d04b789a1
Chitra-Gadekar <61192816+Chitra-Gadekar@users.noreply.github.com>
2020-02-18 18:56:25 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -3,11 +3,11 @@ int main()
 {
    float num1, num2, sum;
    printf("Enter first number: ");
-   scanf("%d", &num1);
+   scanf("%f", &num1);
    printf("Enter second number: ");
-   scanf("%d", &num2);
+   scanf("%f", &num2);
 
    sum = num1 + num2;
-   printf("Sum of the entered numbers: %d", sum);
+   printf("Sum of the entered numbers: %f", sum);
    return 0;
 }
